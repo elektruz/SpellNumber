c07e0c9527b5f4d0d89ed04858b2b5cffda21e9d
elektruz <38428958+elektruz@users.noreply.github.com>
2020-05-28 09:32:52 +0530

    Spell Number
    Spell Number

---
+++ SpellNumber/ApplicationConstants.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SpellNumber
+{
+    public static class ApplicationConstants
+    {
+        public static string[] digitUnits = {
+            "Zero", "One", "Two", "Three","Four", "Five", "Six", "Seven", "Eight", "Nine",
+            "Ten", "Eleven","Twelve", "Thirteen", "Fourteen", "Fifteen", "Sixteen","Seventeen", "Eighteen", "Nineteen"
+        };
+
+        public static string[] tenPositions =
+        {
+            "", "", "Twenty", "Thirty", "Forty","Fifty", "Sixty", "Seventy", "Eighty", "Ninety"
+        };
+    }
+}
---
+++ SpellNumber/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace SpellNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {             
+            while (true)
+            {
+                Console.WriteLine("Enter the number");
+                var userInput = Convert.ToInt64(Console.ReadLine());
+                var words = ConvertNumberTowords(userInput);
+
+                Console.WriteLine(words);                
+
+            }
+        }
+
+        public static string ConvertNumberTowords(Int64 i)
+        {
+            if (i < 20)
+            {
+                return ApplicationConstants.digitUnits[i];
+            }
+            if (i < 100)
+            {
+                return ApplicationConstants.tenPositions[i / 10] + ((i % 10 > 0) ? " " + ConvertNumberTowords(i % 10) : "");
+            }
+            if (i < 1000)
+            {
+                return ApplicationConstants.tenPositions[i / 100] + " Hundred"
+                        + ((i % 100 > 0) ? " And " + ConvertNumberTowords(i % 100) : "");
+            }
+            if (i < 100000)
+            {
+                return ConvertNumberTowords(i / 1000) + " Thousand "
+                + ((i % 1000 > 0) ? " " + ConvertNumberTowords(i % 1000) : "");
+            }
+            if (i < 10000000)
+            {
+                return ConvertNumberTowords(i / 100000) + " Lakh "
+                        + ((i % 100000 > 0) ? " " + ConvertNumberTowords(i % 100000) : "");
+            }
+            return ConvertNumberTowords(i / 10000000) + " Crore "
+                        + ((i % 10000000 > 0) ? " " + ConvertNumberTowords(i % 10000000) : "");
+        }
+    }
+}
